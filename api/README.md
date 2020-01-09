d3ed1378c61cc620c67c9b630df0e394a8e3d1fa
Benjamin Cooper <bencooper222@gmail.com>
2020-01-09 01:55:50 -0600

    Basic finished setup

--- api/README.md
+++
@@ -1,2 +0,0 @@
-# What is this?
-This API will produce random inputs and store them. Students will use this in their problems.
--- api/backend/generators/day2/zero.js
+++
@@ -1,6 +0,0 @@
-const quotes = require('./quotes');
-const { sampleSize, shuffle } = require('lodash');
-
-exports.generate = (length = 100) => {
-  return shuffle(sampleSize(quotes, length));
-};
