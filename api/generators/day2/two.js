8138e6a517ff33cc7fd465900f78eddc0c397b3a
Benjamin Cooper <bencooper222@gmail.com>
2020-01-08 23:24:34 -0600

    Move to backend folder and finish generators

--- api/generators/day2/two.js
+++
@@ -1,9 +0,0 @@
-const pokemon = require('./pokemon.json');
-const { shuffle, sampleSize } = require('lodash');
-const { getRandomInt } = require('../util');
-
-exports.generate = () => {
-  const num = getRandomInt(140, 385);
-
-  return JSON.stringify(shuffle(sampleSize(pokemon, num).map(el => el.name)));
-};
