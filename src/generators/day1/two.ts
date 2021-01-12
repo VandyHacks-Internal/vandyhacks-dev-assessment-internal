b944dcd219d563841377ae474b367788d9c8782f
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-11 23:39:54 -0500

    low key troll but empty out the level 2 inputs because no need

--- src/generators/day1/two.ts
+++ src/generators/day1/two.ts
@@ -1,11 +1,3 @@
-import { getRandomInt } from '../util';
-import faker from 'faker';
-import { format } from 'date-fns';
-
-export function generate() {
-  return JSON.stringify(
-    new Array(getRandomInt(140, 385))
-      .fill(0)
-      .map(_ => format(faker.date.between('2002-01-01', '2020-01-05'), 'yyyy-MM-dd')),
-  );
+export function generate(): string {
+  return 'There is no input for this level. Good luck :)';
 }
