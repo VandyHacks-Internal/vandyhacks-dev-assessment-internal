b6bca823f2257d5f61093b69bbcafa8bbd23592a
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-12 18:47:43 -0500

    level 0 solver

--- src/solvers/day1/zero.ts
+++ src/solvers/day1/zero.ts
@@ -1,12 +1,17 @@
 export function solve(input: string): { answer: number } {
-  const data = JSON.parse(input);
+  const parsedData = input
+    .split('\n')
+    .map(el => {
+      const [_, __, ___, countryCode, ____, _____] = el.split(',');
+      return countryCode;
+    })
+    .slice(1);
+
   let answer = 0;
 
-  for (let { email } of data) {
-    if (email.includes('vanderbilt.edu')) answer++;
+  for (let countryCode of parsedData) {
+    if (countryCode !== 'US') answer++;
   }
 
-  return {
-    answer,
-  };
+  return { answer };
 }
