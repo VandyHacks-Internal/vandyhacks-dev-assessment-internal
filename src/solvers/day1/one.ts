4f4c2a53a31997733c920b3eb9bb368c3ef4f565
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-12 19:59:55 -0500

    level 1 solver and do some typescript stuff to allow spread notation

--- src/solvers/day1/one.ts
+++ src/solvers/day1/one.ts
@@ -1,42 +1,22 @@
-const START = 9 * 3600 + 3 * 60;
-const END = 14 * 3600 + 38 * 60;
-
-interface HourMap {
-  [propName: number]: number;
+interface scoreMap {
+  [name: string]: number;
 }
 
-const updateHourMap = (hour: number, hourMap: HourMap) => {
-  if (hourMap[hour] == null) hourMap[hour] = 1;
-  else hourMap[hour]++;
-};
-
-export function solve(input: string): { answer: { one: number; two: number } } {
-  const parsedData = input
-    .split('\n')
-    .map(el => {
-      const [_, __, ___, checkin] = el.split(',');
-      const [hour, minute, second] = checkin.split(':').map(el => Number(el));
-
-      return [hour, hour * 3600 + minute * 60 + second];
-    })
-    .slice(1);
-
-  let one = 0;
-  const hourMap: HourMap = {};
-
-  for (let [hour, time] of parsedData) {
-    if (START <= time && time <= END) one++;
-    updateHourMap(hour, hourMap);
+export function solve(input: string) {
+  const data = JSON.parse(input);
+  const recalculated: scoreMap = {};
+  for (let { attendees } of data) {
+    const filtered = [...new Set(attendees)];
+    for (let attendee of filtered) {
+      // obligatory type check
+      if (typeof attendee === 'string') {
+        recalculated[attendee] = 1 + (recalculated[attendee] || 0);
+      } else {
+        // Should never get here
+        throw 'Someone (me) made an oopsie. Please contact sam@vandyhacks.org with a scathing bug report.';
+      }
+    }
   }
-
-  const [maxHour] = Object.entries(hourMap).reduce(
-    ([currMaxHour, currMax], [hour, count]) => {
-      if (count > currMax) return [hour, count];
-
-      return [currMaxHour, currMax];
-    },
-    ['', 0],
-  );
-
-  return { answer: { one, two: Number(maxHour) } };
+  console.log(recalculated);
+  return recalculated;
 }
