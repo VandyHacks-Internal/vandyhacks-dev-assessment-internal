73d1c2e0ffdd453284fa0170528ff9f81638629e
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-12 20:25:21 -0500

    level 2 stuff lol

--- src/solvers/day1/two.ts
+++ src/solvers/day1/two.ts
@@ -1,22 +1,7 @@
-import fetch from 'node-fetch';
+interface answer {
+  [ugh: string]: string;
+}
 
-const API_URL = 'https://api.exchangeratesapi.io/';
-const CURRENCY = 'GBP';
-
-const getApiURL = (date: string) => {
-  return `${API_URL}${date}?symbols=${CURRENCY}`;
-};
-
-const fetchByDate = async (date: string): Promise<number> => {
-  return (await (await fetch(getApiURL(date))).json()).rates[CURRENCY];
-};
-
-export async function solve(input: string): Promise<{ answer: number[] }> {
-  const dates = JSON.parse(input);
-
-  return {
-    answer: await Promise.all(
-      dates.map(async (date: string): Promise<number> => fetchByDate(date)),
-    ),
-  };
+export function solve(input: string) {
+  return { 'maybe the': 'real answer was', 'the friends': 'we', 'made along': 'the way' };
 }
