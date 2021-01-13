cfd9972911990e674bae990f7a4a73f267792c29
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-12 23:31:30 -0500

    sm0l hack to hide answers on prod while I figure out how Express works

--- src/solvers/day1/one.ts
+++ src/solvers/day1/one.ts
@@ -1,22 +1,25 @@
-interface scoreMap {
-  [name: string]: number;
-}
+// interface scoreMap {
+//   [name: string]: number;
+// }
 
+// export function solve(input: string) {
+//   const data = JSON.parse(input);
+//   const recalculated: scoreMap = {};
+//   for (let { attendees } of data) {
+//     const filtered = [...new Set(attendees)];
+//     for (let attendee of filtered) {
+//       // obligatory type check
+//       if (typeof attendee === 'string') {
+//         recalculated[attendee] = 1 + (recalculated[attendee] || 0);
+//       } else {
+//         // Should never get here
+//         throw 'Someone (me) made an oopsie. Please contact sam@vandyhacks.org with a scathing bug report.';
+//       }
+//     }
+//   }
+
+//   return { answer: recalculated };
+// }
 export function solve(input: string) {
-  const data = JSON.parse(input);
-  const recalculated: scoreMap = {};
-  for (let { attendees } of data) {
-    const filtered = [...new Set(attendees)];
-    for (let attendee of filtered) {
-      // obligatory type check
-      if (typeof attendee === 'string') {
-        recalculated[attendee] = 1 + (recalculated[attendee] || 0);
-      } else {
-        // Should never get here
-        throw 'Someone (me) made an oopsie. Please contact sam@vandyhacks.org with a scathing bug report.';
-      }
-    }
-  }
-
-  return { answer: recalculated };
+  return { answer: 'Nice try. Check back here after your submission deadline!' };
 }
--- src/solvers/day1/zero.ts
+++ src/solvers/day1/zero.ts
@@ -1,19 +1,23 @@
-export function solve(input: string): { answer: number } {
-  const parsedData = input
-    .split('\n')
-    .map(el => {
-      const [_, __, ___, countryCode, ____, _____] = el.split(',');
-      return countryCode;
-    })
-    .slice(1);
+// export function solve(input: string): { answer: number } {
+//   const parsedData = input
+//     .split('\n')
+//     .map(el => {
+//       const [_, __, ___, countryCode, ____, _____] = el.split(',');
+//       return countryCode;
+//     })
+//     .slice(1);
 
-  let answer = 0;
+//   let answer = 0;
 
-  for (let countryCode of parsedData) {
-    if (countryCode !== 'US') {
-      answer++;
-    }
-  }
+//   for (let countryCode of parsedData) {
+//     if (countryCode !== 'US') {
+//       answer++;
+//     }
+//   }
 
-  return { answer };
+//   return { answer };
+// }
+
+export function solve(input: string) {
+  return { answer: 'Nice try. Check back here after your submission deadline!' };
 }
