2dcd8d6a2725c7d9415ba60e6610152e32ce29ff
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-14 00:18:27 -0500

    toLowerCase all usernames so it's flexible

--- api/index.ts
+++ api/index.ts
@@ -8,23 +8,23 @@ const createPrefixedPath = (path = '') => `/api/${path}`;
 
 // default to level 0 for both
 app.get(createPrefixedPath('solve/:user'), async (req, res) => {
-  return res.redirect(createPrefixedPath(`solve/${req.params.user}/0`));
+  return res.redirect(createPrefixedPath(`solve/${req.params.user.toLowerCase()}/0`));
 });
 
 app.get(createPrefixedPath('generate/:user'), async (req, res) => {
-  return res.redirect(createPrefixedPath(`generate/${req.params.user}/0`));
+  return res.redirect(createPrefixedPath(`generate/${req.params.user.toLowerCase()}/0`));
 });
 
 const defaultHandler = async (req: Request, res: Response, requestType: SolveOrGenerate) => {
-  if (req.params.user == undefined) return res.status(500).end('No user specified');
+  if (req.params.user.toLowerCase() == undefined) return res.status(500).end('No user specified');
 
   const level = Number(req.params.level) as Level;
   try {
     if (requestType === 'solve') {
-      const rtn = await getProblemSolution(req.params.user, level);
+      const rtn = await getProblemSolution(req.params.user.toLowerCase(), level);
       return res.json(rtn); // Answer format will always be JSON
     } else {
-      const rtn = await retrieveGeneratedData(req.params.user, level);
+      const rtn = await retrieveGeneratedData(req.params.user.toLowerCase(), level);
       return res.end(rtn);
     }
   } catch (err) {
