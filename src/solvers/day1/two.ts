0999d7bf9b49c0f4bc211964f35f34257c40eb73
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-21 01:29:00 -0500

    update day 2 solver

--- src/solvers/day1/two.ts
+++ src/solvers/day1/two.ts
@@ -1,22 +1,38 @@
-interface scoreMap {
-  [name: string]: number;
-}
-
 export function solve(input: string) {
   const data = JSON.parse(input);
-  const recalculated: scoreMap = {};
+  const cheaters: Map<string, 1> = new Map();
+  const recalculated: Map<string, number> = new Map();
+  let highestScore: number = 0;
   for (let { attendees } of data) {
-    const filtered = [...new Set(attendees)];
-    for (let attendee of filtered) {
-      // obligatory type check
-      if (typeof attendee === 'string') {
-        recalculated[attendee] = 1 + (recalculated[attendee] || 0);
+    const realAttendees: Map<string, 1> = new Map();
+    for (let attendee of attendees) {
+      if (realAttendees.has(attendee) && !cheaters.has(attendee)) {
+        cheaters.set(attendee, 1);
       } else {
-        // Should never get here
-        throw 'Someone (me) made an oopsie. Please contact sam@vandyhacks.org with a scathing bug report.';
+        const score: number = 1 + (recalculated.get(attendee) || 0);
+        highestScore = highestScore = score > highestScore ? score : highestScore;
+        recalculated.set(attendee, score);
       }
+      realAttendees.set(attendee, 1);
     }
   }
-
-  return { answer: recalculated };
+  // Get highest scoring hackers
+  const highestScoringHackers: string[] = [];
+  for (let [k, v] of recalculated.entries()) {
+    if (v === highestScore) {
+      highestScoringHackers.push(k);
+    }
+  }
+  console.log('cheaters:', cheaters);
+  const cheaterNames = [...cheaters.keys()];
+  cheaterNames.sort();
+  highestScoringHackers.sort();
+  const output = {
+    answer: {
+      hackers_illegal: cheaterNames,
+      hackers_highest: { score: highestScore, hackers: highestScoringHackers },
+    },
+  };
+  console.log(output);
+  return output;
 }
