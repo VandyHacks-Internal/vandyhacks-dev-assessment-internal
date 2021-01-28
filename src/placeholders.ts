d4429bed84802b0758329447fc14eca365ce1782
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-28 13:04:08 -0600

    more accurate placeholder message

--- src/placeholders.ts
+++ src/placeholders.ts
@@ -3,5 +3,5 @@ import { Solution } from './types';
 export const generateLock: string =
   'Not open for business yet. Check back here when your assignment starts!';
 export const solveLock: Solution = {
-  answer: 'Nice try. Check back here after your submission deadline!',
+  answer: 'Thanks for checking. We will be releasing answers to all inputs for day 2 soon!',
 };
